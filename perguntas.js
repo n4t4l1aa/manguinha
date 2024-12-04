dfa5eea2194d1d8370b3aa11dfcde0e6628df70e
Natália! <natieusouza10@gmail.com>
2024-12-03 21:46:42 -0300

    perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+     'Programação', 'Programação',
+     'O que é Python?', 'O que é Python?',
+     'O Python é uma linguagem de programação' 'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+     'Geografia', 'Geografia',
+     'Qual a capital da França?', 'Qual a capital da França?',
+     'A capital da França é Paris' 'A capital da França é Paris'
+)
+
+criaCartao(
+     'Programação', 'Programação',
+     'O que é uma função?', 'O que é uma função?',
+     'Uma função é um bloco de código que executa alguma tarefa' 'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+     'Lingua inglesa', 'Lingua inglesa',
+     'Como se diz oi em Inglês?', 'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
